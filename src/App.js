ae11e742895358c02e0d7da346030180a9e33822
qkrthdus605 <qkrthdus605@gmail.com>
2021-10-12 00:36:25 +0900

    실습 : 9장 - 컴포넌트 스타일링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <StyledComponent />
     </div>
   );
 }
